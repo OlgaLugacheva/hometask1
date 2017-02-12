649184dd13e91281165a653f5a6dbb5c032811f2
OlgaLugacheva <lugachevaolga@gmail.com>
2017-02-12 19:48:53 +0500

    Initial commit

---
+++ src/Symbols.java
@@ -0,0 +1,7 @@
+/**
+ * Created by Olga on 12.02.2017.
+ */
+public class Symbols {
+    public final static String PUNCTUATION_MARKS = ",|!|\\?|\\n|\\r|\\s|\\.|-|\\(|\\)";
+    public final static String SYMBOLS = "[а-яА-ЯЁё0-9]+";
+}
